181a6222fdbcff3cb322833e054f72433ee695dc
Lukasz Czajka <lukaszcz@mimuw.edu.pl>
2026-02-07 22:42:15 +0100

    fix scripts/brsync.sh

--- scripts/brsync.sh
+++ scripts/brsync.sh
@@ -2,7 +2,10 @@
 set -euo pipefail
 
 git fetch --prune
-for branch in $(git branch -r --no-merged origin/main | grep -v '\->'); do
+for branch in $(git for-each-ref --format='%(refname:short)' --no-merged=origin/main refs/remotes/origin); do
+  if [[ "$branch" == "origin/HEAD" ]]; then
+    continue
+  fi
   local_branch="${branch#origin/}"
   if ! git show-ref --verify --quiet "refs/heads/$local_branch"; then
     git branch --track "$local_branch" "$branch"
